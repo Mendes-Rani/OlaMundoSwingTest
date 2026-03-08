ec59b234250806a7aea7cc2e704cc603e0bfecba
Rani <ranieridasilvamendes159@gmail.com>
2026-03-08 19:00:38 -0300

    primeiro-commit

--- src/Cursoemvideo.java
+++ src/Cursoemvideo.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class cursoemvideo {
+public class Cursoemvideo extends JFrame{
+    private JLabel lblMensagem;
+    private JButton btnClick;
+    private JPanel painelMenu;
+
+    public Cursoemvideo() {
+        btnClick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lblMensagem.setText("Olá, Mundo");
+                //JOptionPane.showMessageDialog(btnClick, "Olá, Mundo!");
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Cursoemvideo frame = new Cursoemvideo();
+                frame.setContentPane(frame.painelMenu);
+                frame.setTitle("Cursoemvideo");
+                frame.setSize(400,400);
+                frame.setVisible(true);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            }
+        });
+
+    }
 }
